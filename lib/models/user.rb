b01aae0d2f26bbca16dd97380170b447d8affa2a
github username <github email address>
2019-06-18 12:57:17 -0600

    fixed plural validation

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -3,5 +3,5 @@ require 'pry'
 class User < ActiveRecord::Base
     has_many :favorites
     has_many :dinosaurs, through: :favorites
-    validate :name, uniqueness: true
+    validates :name, uniqueness: true
 end
